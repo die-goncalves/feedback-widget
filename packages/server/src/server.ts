6415f62f627ed5c47e2c00a13d183cea6d0f32bf
die-goncalves <die.goncalves1990@gmail.com>
2022-05-11 14:07:17 -0300

    Criar arquivo para as rotas da aplicação

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,45 +1,10 @@
 import express from 'express'
-import { prisma } from './prisma'
-import nodemailer from 'nodemailer'
+import { routes } from './routes'
 
 const app = express()
 
 app.use(express.json())
-
-const transport = nodemailer.createTransport({
-  host: 'smtp.mailtrap.io',
-  port: 2525,
-  auth: {
-    user: process.env.EMAIL_USERNAME,
-    pass: process.env.EMAIL_PASSWORD
-  }
-})
-
-app.post('/feedbacks', async (req, res) => {
-  const { type, comment, screenshot } = req.body
-
-  const feedback = await prisma.feedback.create({
-    data: {
-      type,
-      comment,
-      screenshot
-    }
-  })
-
-  await transport.sendMail({
-    from: 'Equipe Feedget <oi@feedget.com>',
-    to: 'Diego Gonçalves <die.goncalves1990@gmail.com>',
-    subject: 'Novo feedback',
-    html: [
-      `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
-      `<p>Tipo de feedback: ${type}</p>`,
-      `<p>Comentário: ${comment}</p>`,
-      `</div>`
-    ].join('\n')
-  })
-
-  return res.status(201).send({ data: feedback })
-})
+app.use(routes)
 
 app.listen(3333, () => {
   console.log('HTTP server running!!')
