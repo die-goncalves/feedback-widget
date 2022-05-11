20139055dc0c17873e88a050a7038b8ab64d1a0d
die-goncalves <die.goncalves1990@gmail.com>
2022-05-11 11:31:54 -0300

    Adicionar Nodemailer para envio de email

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,9 +1,19 @@
 import express from 'express'
 import { prisma } from './prisma'
+import nodemailer from 'nodemailer'
 
 const app = express()
 
 app.use(express.json())
+
+const transport = nodemailer.createTransport({
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  auth: {
+    user: process.env.EMAIL_USERNAME,
+    pass: process.env.EMAIL_PASSWORD
+  }
+})
 
 app.post('/feedbacks', async (req, res) => {
   const { type, comment, screenshot } = req.body
@@ -14,6 +24,18 @@ app.post('/feedbacks', async (req, res) => {
       comment,
       screenshot
     }
+  })
+
+  await transport.sendMail({
+    from: 'Equipe Feedget <oi@feedget.com>',
+    to: 'Diego Gonçalves <die.goncalves1990@gmail.com>',
+    subject: 'Novo feedback',
+    html: [
+      `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
+      `<p>Tipo de feedback: ${type}</p>`,
+      `<p>Comentário: ${comment}</p>`,
+      `</div>`
+    ].join('\n')
   })
 
   return res.status(201).send({ data: feedback })
