feef50c5786c22a0a405e40b42613e4f2b623e17
die-goncalves <die.goncalves1990@gmail.com>
2022-05-12 10:03:41 -0300

    Adicionar botão do widget

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,7 +1,17 @@
+import { ChatTeardropDots } from 'phosphor-react'
+
 function App() {
   return (
-    <div className="flex bg-black text-white justify-center items-center">
-      Feedget
+    <div className="absolute bottom-4 right-4 flex flex-col items-end gap-2">
+      <div className="flex rounded-full justify-end cursor-pointer bg-transparent w-[8.1875rem] h-12 group">
+        <button className="flex p-3 w-12 h-12 bg-brand rounded-full group-hover:w-[8.1875rem] drop-shadow-[0_0.5rem_1.5rem_rgba(130,87,229,0.25)] border-none transition-[width] duration-300 ease-out">
+          <ChatTeardropDots className="w-6 h-6" weight="bold" color="#ffffff" />
+
+          <span className="absolute top-3 right-3 opacity-0 group-hover:opacity-100 transition-[opacity] duration-300 ease-out">
+            <span className="pl-2 text-white font-medium">Feedback</span>
+          </span>
+        </button>
+      </div>
     </div>
   )
 }
