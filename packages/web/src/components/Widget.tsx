f59a4bd27f12944c82be1a00377b760a4c5c8553
die-goncalves <die.goncalves1990@gmail.com>
2022-05-16 10:10:23 -0300

    Atualizar o componente Widget

--- packages/web/src/components/Widget.tsx
+++ packages/web/src/components/Widget.tsx
@@ -1,15 +1,20 @@
 import { ChatTeardropDots } from 'phosphor-react'
 import { Popover } from '@headlessui/react'
+import { WidgetContent } from './WidgetContent'
 
 export function Widget() {
   return (
     <Popover className="absolute bottom-4 right-4 flex flex-col items-end gap-2">
       {({ open }) => (
         <>
+          <Popover.Panel>
+            <WidgetContent />
+          </Popover.Panel>
+
           <div className="flex rounded-full justify-end cursor-pointer bg-transparent w-[8.1875rem] h-12 group">
             <Popover.Button
-              title="Widget de feedback"
-              className={`flex p-3 w-12 h-12 bg-brand hover:bg-brand-hover rounded-full group-hover:w-[8.1875rem] drop-shadow-[0_0.5rem_1.5rem_rgba(130,87,229,0.25)]  border-none transition-all duration-300 ease-out focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-dark/surface-primary ring-brand ${
+              aria-label="Feedback"
+              className={`flex p-3 w-12 h-12 bg-brand hover:bg-brand-hover rounded-full group-hover:w-[8.1875rem] drop-shadow-[0_0.5rem_1.5rem_rgba(130,87,229,0.25)]  border-none transition-all duration-300 ease-out focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-dark/surface-primary focus:ring-brand ${
                 open && 'bg-brand-hover'
               }`}
             >
