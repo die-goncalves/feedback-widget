ec0fd0d773d6ebf7508013217d8b2a27d03fea38
die-goncalves <die.goncalves1990@gmail.com>
2022-05-18 14:20:41 -0300

    Adicionar tema claro, escuro e padrão do sistema

--- packages/web/src/components/SuccessFeedback.tsx
+++ packages/web/src/components/SuccessFeedback.tsx
@@ -9,7 +9,7 @@ export function SuccessFeedback({ restartFeedback }: SuccessFeedbackProps) {
     <div className="flex flex-col mt-auto mb-10">
       <div className="flex flex-col gap-2">
         <img src={Success} alt="Sucesso" className="w-10 mx-auto" />
-        <h1 className="text-[1.25rem] leading-[1.5rem] font-medium text-dark/text-primary">
+        <h1 className="text-[1.25rem] leading-[1.5rem] font-medium text-light/text-primary dark:text-dark/text-primary">
           Agradecemos o feedback
         </h1>
       </div>
@@ -17,7 +17,7 @@ export function SuccessFeedback({ restartFeedback }: SuccessFeedbackProps) {
       <button
         autoFocus
         onClick={restartFeedback}
-        className="mt-6 mx-auto px-6 py-2 rounded-[0.25rem] bg-dark/surface-secondary text-[0.875rem] leading-[1.5rem] font-medium text-dark/text-primary hover:bg-dark/surface-secondary-hover focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-dark/surface-primary focus:ring-brand transition-all duration-300 ease-out"
+        className="mt-6 mx-auto px-6 py-2 rounded-[0.25rem] bg-light/surface-secondary dark:bg-dark/surface-secondary text-[0.875rem] leading-[1.5rem] font-medium text-light/text-primary dark:text-dark/text-primary hover:bg-light/surface-secondary-hover dark:hover:bg-dark/surface-secondary-hover focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-light/surface-primary dark:focus:ring-offset-dark/surface-primary focus:ring-brand transition-all duration-300 ease-out"
       >
         Quero enviar outro
       </button>
--- packages/web/src/components/Widget.tsx
+++ packages/web/src/components/Widget.tsx
@@ -4,33 +4,35 @@ import { WidgetContent } from './WidgetContent'
 
 export function Widget() {
   return (
-    <Popover className="absolute bottom-4 right-4 flex flex-col items-end gap-2">
-      {({ open }) => (
-        <>
-          <Popover.Panel>
-            <WidgetContent />
-          </Popover.Panel>
+    <div className="fixed bottom-4 right-4 ">
+      <Popover className="flex flex-col items-end gap-2">
+        {({ open }) => (
+          <>
+            <Popover.Panel className="w-[21rem] h-[16.5rem] bg-light/surface-primary dark:bg-dark/surface-primary p-4 rounded-[1rem] relative flex flex-col items-center shadow-lg ring-1 ring-black ring-opacity-5">
+              <WidgetContent />
+            </Popover.Panel>
 
-          <div className="flex rounded-full justify-end cursor-pointer bg-transparent w-[8.1875rem] h-12 group">
-            <Popover.Button
-              aria-label="Feedback"
-              className={`flex p-3 w-12 h-12 bg-brand hover:bg-brand-hover rounded-full group-hover:w-[8.1875rem] drop-shadow-[0_0.5rem_1.5rem_rgba(130,87,229,0.25)]  border-none transition-all duration-300 ease-out focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-dark/surface-primary focus:ring-brand ${
-                open && 'bg-brand-hover'
-              }`}
-            >
-              <ChatTeardropDots
-                className="w-6 h-6"
-                weight="bold"
-                color="#ffffff"
-              />
+            <div className="flex rounded-full justify-end cursor-pointer bg-transparent w-[8.1875rem] h-12 group">
+              <Popover.Button
+                aria-label="Feedback"
+                className={`flex p-3 w-12 h-12 bg-brand hover:bg-brand-hover rounded-full group-hover:w-[8.1875rem] drop-shadow-[0_0.5rem_1.5rem_rgba(130,87,229,0.25)] border-none transition-all duration-300 ease-out outline-none focus:outline-offset-2 focus:outline focus:outline-2 focus:outline-brand ${
+                  open && 'bg-brand-hover'
+                }`}
+              >
+                <ChatTeardropDots
+                  className="w-6 h-6"
+                  weight="bold"
+                  color="#ffffff"
+                />
 
-              <span className="absolute top-3 right-3 opacity-0 group-hover:opacity-100 transition-[opacity] duration-300 ease-out">
-                <span className="pl-2 text-white font-medium">Feedback</span>
-              </span>
-            </Popover.Button>
-          </div>
-        </>
-      )}
-    </Popover>
+                <span className="absolute top-3 right-3 opacity-0 group-hover:opacity-100 transition-[opacity] duration-300 ease-out">
+                  <span className="pl-2 text-white font-medium">Feedback</span>
+                </span>
+              </Popover.Button>
+            </div>
+          </>
+        )}
+      </Popover>
+    </div>
   )
 }
